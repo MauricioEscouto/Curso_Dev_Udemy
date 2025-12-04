218b25f5da5c5055685ff8e3fc63d0a7b7581a7d
Mauricio Escouto <mauricioescouto9953@gmail.com>
2025-12-04 00:19:47 -0300

    implementado função para obter projetos vinculados ao Account e renderizar no componente listarProjetos

--- force-app/main/default/lwc/listarProjetos/listarProjetos.js
+++ force-app/main/default/lwc/listarProjetos/listarProjetos.js
@@ -1,5 +1,28 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement, wire } from 'lwc';
+import obterProjetosService from '@salesforce/apex/AccountService.obterProjetos';
 
 export default class ListarProjetos extends LightningElement {
-    
+    @api recordId;
+
+    projetos_obtidos;
+    error;
+
+    @wire(obterProjetosService, { accountId: '$recordId' })
+    obterProjetos({error, data}) {
+    if (data) {
+      this.projetos_obtidos = data.map((element) => {
+        return {
+          projeto: {
+            Id: element.Id,
+            Name: element.Name,
+            Data_de_Inicio__c: element.DataInicio__c,
+            Status__c: element.Status__c
+          }
+        };
+      });
+
+    } else if (error) {
+      this.error = error;
+    }
+  }
 }
